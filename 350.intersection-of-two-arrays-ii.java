4c01e40383ed900ba68fead89e7cb1e2254bce14
kartik-04 <04kartik04@gmail.com>
2025-11-06 10:07:03 +0530

    Maximum-subarray using Kaden's algorithm

---
+++ 350.intersection-of-two-arrays-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=350 lang=java
+ *
+ * [350] Intersection of Two Arrays II
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+    }
+}
+// @lc code=end
+
--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
+                        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+            for(int i = 0; i < nums.length; i++){
+                    if(currSum < 0){
+                        currSum = 0;
+                    }
+                    currSum += nums[i];
+                maxSum = Math.max(currSum, maxSum);
         
+        }
+     return maxSum;     
     }
 }
 // @lc code=end
