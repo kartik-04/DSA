c9675fb3d64d7de1fa2347fda3e70d667ef22c46
kartik-04 <04kartik04@gmail.com>
2025-11-07 14:04:28 +0530

    implement initial structure for Product of Array Except Self solution

--- 350.intersection-of-two-arrays-ii.java
+++ 350.intersection-of-two-arrays-ii.java
@@ -7,7 +7,30 @@
 // @lc code=start
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
+        List<Integer> ans = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums1.length; i++){
+            if(!map.containsKey(nums1[i])){
+                map.put(nums1[i], 1);
+            }else{
+                map.put(nums1[i], map.get(nums1[i])+1);
+            }
+        }
+
+        for(int i = 0; i < nums2.length; i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0){
+                ans.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i])-1);
+                if(map.get(nums2[i]) == 0){
+                    map.remove(nums2[i]);
+                }
+            }
+        }
+        int[] res = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++){
+            res[i] = ans.get(i);
+        }
+        return res;
     }
 }
 // @lc code=end
